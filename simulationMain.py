78f735124b58c826c592d698bf3684937342375f
APProGamers <89620666+Batface848@users.noreply.github.com>
2024-03-31 23:37:10 +0800

    Add files via upload

--- simulationMain.py
+++ simulationMain.py
@@ -17,9 +17,16 @@ viz.go()
 class LHCSimulation:
     def __init__(self):
         vizshape.addGrid(step = 1.0) # Adds a grid for easier testing
-        self.collider = Collider(ORIGIN)
-        self.protonOne = Proton([0, 0, 5], PURPLE, PROTON_RADIUS, self) # Test proton
-        self.protonTwo = Proton([0, 0, 10], GREEN, PROTON_RADIUS, self)
+        viz.MainView.collision(viz.OFF)
+        self.points = []
+        self.GUIObjects = []
+        self.gasPump = GasPump([0, 5, 20], BLUE, (3, 10, 3))
+        self.sourceChamber = SourceChamber([0, 5, 15.5], CYAN, 1, 6)
+        self.collider = Collider([30,0,30], BLUE, 10, 3)
+        self.pointOne = HydrogenAtom([10, 0, 10], SIMULATED_PROTON_RADIUS, [0, 0, 0], [-1, 0, 0]) # Test point
+        self.points.append(self.pointOne)
+        self.pointTwo = HydrogenAtom([-10, 0, 10], SIMULATED_PROTON_RADIUS, [0, 0, 0], [1, 0, 0])
+        self.points.append(self.pointTwo)
         self.centreBall = vizshape.addSphere()
         self.cameraPos = [10, 10, 10] # x, y, z
         self.cameraAngle = [0, 0, 0] # Yaw, pitch, roll
@@ -32,23 +39,73 @@ class LHCSimulation:
 
     def run(self):
         '''Runner method'''
-        self.frames += 1
         viz.MainView.setPosition(self.cameraPos) # Camera
         viz.MainView.setEuler(self.cameraAngle)
         self.drawSprites()
-        self.protonOne.move()
-        self.protonTwo.move()
+        for point in self.points:
+            point.enablePhysics()
+        self.checkPointCollisionDetection()
+        self.drawGUI()
+        viz.callback(viz.BUTTON_EVENT, self.getGUIState)
+        self.frames += 1
 
     def drawSprites(self):
         '''Method that draws all sprites required from the sprites file'''
-        self.protonOne.draw()
-        self.protonTwo.draw()
+        for point in self.points:
+            point.draw()
+        self.gasPump.draw()
+        self.sourceChamber.draw()
+        self.collider.draw()
         
     def updateCam(self):
-        '''Update camera position based on the camera's new position'''
+        '''Method that updates the camera position based on the camera's new position'''
         self.cameraPos = viz.MainView.getPosition()
         self.cameraAngle = viz.MainView.getEuler()
         self.cameraAngle[2] = 0 # Prevent camera angle tilting
+        
+    def drawGUI(self):
+        '''Method that draws the interactive GUI on the screen'''
+        self.spawnButton = viz.addButtonLabel("Add Hydrogen")
+        self.spawnButton.setPosition(0.1, 0.95)
+        self.GUIObjects.append(self.spawnButton)
+        # self.spawnButton.color(GREEN)
+        
+    def getGUIState(self, obj, state):
+        for obj in self.GUIObjects:
+            if obj == self.spawnButton:
+                if state == viz.DOWN:
+                    self.spawnHydrogen()
+                else:
+                    pass
+        
+        
+    def spawnHydrogen(self):
+        '''Method that allows a hydrogen atom to be "pumped" from the gas pump'''
+        hydrogenPoint = HydrogenAtom([0, 5, 18.5], SIMULATED_PROTON_RADIUS, [0, 0, -0.01], [0, 0, -1])
+        self.points.append(hydrogenPoint)
+
+    def checkPointCollisionDetection(self):
+        '''Method that checks for collision detection between all points'''
+        self.distanceMatrix = []
+        for i in range(len(self.points)):
+            rowVector = []
+            for j in range(len(self.points)):
+                if i == j:
+                    rowVector.append(-1)
+                else:
+                    distance = getMagnitude(self.points[i].pos, self.points[j].pos)
+                    rowVector.append(distance)
+            self.distanceMatrix.append(rowVector)
+
+        for i in range(len(self.distanceMatrix)):
+            for j in range(i, len(self.distanceMatrix)):
+                if self.distanceMatrix[i][j] <= (self.points[i].radius + self.points[j].radius) and self.distanceMatrix[i][j] >= 0:
+                    print("COLLIDE")
+                    
+                        
+        
+
+
 
 engine = LHCSimulation()
 vizact.ontimer(TIME_PERIOD, engine.main) # Final line of code
--- sprites.py
+++ sprites.py
@@ -7,38 +7,79 @@ from mathematicalMethods import *
 import math
 import copy
 
-class Proton:
-    def __init__(self, pos, colour, radius, simulation):
+class Point:
+    def __init__(self, pos, colour, radius, initialVelocity, initialForce):
         self.object = vizshape.addSphere()
         self.colour = colour
         self.radius = radius
-        self.simulation = simulation
         
         self.pos = pos
-        self.orbitalCentre = ORIGIN
-        self.orbitalRadius = getMagnitude(self.orbitalCentre, self.pos)
-        self.oldPos = [pos[0] - 0.01, pos[1], pos[2]]
+        self.oldPos = copy.deepcopy(self.pos)
+        for axis in range(3):
+            self.oldPos[axis] -= initialVelocity[axis]
         self.velocity = [0, 0, 0]
         self.acceleration = [0, 0, 0]
-        self.mass = PROTON_MASS
-        self.force = [0, 0, 0]
+        self.mass = SIMULATED_PROTON_MASS
+        self.force = initialForce
         self.volume = 4/3 * math.pi * ((self.radius) ** 3)
         self.density = self.mass / self.volume
-        self.magneticForce = [0, 0, 0]
-        self.electrostaticForce = [0, 0, 0]
-        self.startTime = 0
-
-        self.simulation = simulation
-        self.collider = self.simulation.collider
         
-    def move(self):
-        '''Move the proton'''
+    def enablePhysics(self):
+        '''Method that enables the point's physics'''
+        self.enableNewtonianMechanics()
+
+    def enableNewtonianMechanics(self):
+        '''Method that moves the point using the calculated resultant force'''
         # Basic Newtonian Mechanics (Kinematics, Dynamics, Verlet Integration)
         # 1. Obtain acceleration by F = ma
         # 2. Obtain velocity by difference in pos and old pos over time
         # 3. Update pos based on s = vt
         # 4. Don't change position directly, change velocity so use oldPos (which is a record of the position of the proton before the frame) -- Analogy: A slingshot; the further you pull the ball back, the higher the velocity of projection
-        self.nonUniformCircularMotion()
+        for axis in range(3):
+            self.acceleration[axis] = self.force[axis] / self.mass
+            self.oldPos[axis] -= self.acceleration[axis] / (RATE_OF_CALCULATIONS ** 2)
+            self.velocity[axis] = (self.pos[axis] - self.oldPos[axis]) * RATE_OF_CALCULATIONS # Rate of calculations used to reduce floating point errors
+        self.oldPos = copy.deepcopy(self.pos)
+        for axis in range(3):
+            self.pos[axis] += self.velocity[axis] / RATE_OF_CALCULATIONS
+        
+    def draw(self):
+        '''Method that draws the point'''
+        self.object.setPosition(self.pos)
+        self.object.color((self.colour))
+        self.object.setScale((self.radius, self.radius, self.radius)) # Allows changing of sphere orientation
+
+class HydrogenAtom(Point):
+    def __init__(self, pos, radius, initialVelocity, initialForce):
+        super().__init__(pos, RED, radius, initialVelocity, initialForce)
+
+    def enablePhysics(self):
+        '''Method that enables the hydrogen atom's physics'''
+        super().enablePhysics()
+
+    def enableNewtonianMechanics(self):
+        '''Method that moves the point using the calculated resultant force'''
+        super().enableNewtonianMechanics()
+
+    def draw(self):
+        '''Method that draws the hydrogen atom'''
+        super().draw()
+
+class Proton(Point):
+    def __init__(self, pos, radius, initialVelocity, initialForce):
+        super().__init__(pos, PURPLE, radius, initialVelocity, initialForce)
+        self.orbitalCentre = ORIGIN
+        self.orbitalRadius = getMagnitude(self.orbitalCentre, self.pos)
+        self.charge = SIMULATED_PROTON_CHARGE
+        self.magneticForce = [0, 0, 0]
+        self.electrostaticForce = [0, 0, 0]
+
+    def enablePhysics(self):
+        '''Method that enables the hydrogen atom's physics'''
+        super().enablePhysics()
+
+    def enableNewtonianMechanics(self):
+        '''Method that moves the point using the calculated resultant force'''
         for axis in range(3):
             self.force[axis] = self.magneticForce[axis] + self.electrostaticForce[axis]
             self.acceleration[axis] = self.force[axis] / self.mass
@@ -48,7 +89,7 @@ class Proton:
         for axis in range(3):
             self.pos[axis] += self.velocity[axis] / RATE_OF_CALCULATIONS
 
-    def uniformCircularMotion(self):
+    def enableUniformCircularMotion(self):
         '''Method that manages uniform circular motion'''
         # Uniform Circular Motion
         # 1. Get direction and angle
@@ -71,18 +112,55 @@ class Proton:
         self.uniformCircularMotion()
         resultantForce = 0.1
         self.electrostaticForce = [directionOfMotion[0] * resultantForce * math.cos(angleOfMotion), 0, directionOfMotion[1] * resultantForce * math.sin(angleOfMotion)]
-        print(self.electrostaticForce)
-        
+
+    def getElectrostaticForce(self):
+        '''Method that obtains the electrostatic force which causes the acceleration of the protons'''
+        for axis in range(3):
+            self.electrostaticForce[axis] = 3
 
     def draw(self):
-        '''Draw the proton'''
+        '''Method that draws the hydrogen atom'''
+        super().draw()
+        
+class GasPump:
+    def __init__(self, pos, colour, size):
+        self.pos = pos
+        self.colour = colour
+        self.size = size
+        self.object = vizshape.addBox(size = size, top = True)
+        
+    def draw(self):
         self.object.setPosition(self.pos)
-        self.object.color((self.colour))
-        self.object.setScale((self.radius, self.radius, self.radius)) # Allows changing of sphere orientation
+        self.object.color(self.colour)
+        self.object.alpha(0.8)
+        
+class SourceChamber:
+    def __init__(self, pos, colour, radius, height):
+        self.pos = pos
+        self.colour = colour
+        self.radius = radius
+        self.height = height
+        self.object = vizshape.addCylinder(height = self.height, radius = self.radius, axis = vizshape.AXIS_Z)
+        
+    def draw(self):
+        '''Method that draws the source chamber'''
+        self.object.setPosition(self.pos)
+        self.object.color(self.colour)
+        self.object.alpha(0.5)
 
 class Collider:
-    def __init__(self, pos):
-        self.object = vizshape.addTorus(radius = 20, tubeRadius = 3)
+    def __init__(self, pos, colour, radius, tubeRadius):
         self.pos = pos # Centre
+        self.colour = colour
+        self.radius = radius
+        self.tubeRadius = tubeRadius
+        self.object = vizshape.addTorus(radius = self.radius, tubeRadius = self.tubeRadius)
+
+    def draw(self):
+        '''Method that draws the collider'''
+        self.object.setPosition(self.pos)
+        self.object.color((self.colour))
+        self.object.alpha(0.5)
+
         
     
